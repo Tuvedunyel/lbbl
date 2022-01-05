ad69f18ae08ceaaad3ea19905e71737bd7dfabb7
Grégoire <72552360+Tuvedunyel@users.noreply.github.com>
2022-01-05 09:32:44 +0100

    burger

--- js/script.js
+++ js/script.js
@@ -1,6 +1,25 @@
-const menuBtn = document.querySelector(".menu-btn");
-const menu = document.getElementById("menu-main-menu");
+const windowWidth = window.innerWidth;
 
-menuBtn.addEventListener("click", () => {
-  menu.classList.toggle("show");
-});
+// menuBtn.addEventListener("click", () => {
+//   menu.classList.toggle("show");
+// });
+
+if (windowWidth <= 1418) {
+  const menuBtn = document.querySelector(".menu-btn");
+  const menu = document.getElementById("menu-main-menu");
+
+  let menuOpen = false;
+  menuBtn.addEventListener("click", () => {
+    if (!menuOpen) {
+      menuBtn.classList.add("open");
+      menuOpen = true;
+      menu.classList.add("show");
+    } else {
+      menuBtn.classList.remove("open");
+      menuOpen = false;
+      menu.classList.remove("show");
+    }
+  });
+
+  const menuBtnBurger = document.querySelector(".menu-btn__burger");
+}
