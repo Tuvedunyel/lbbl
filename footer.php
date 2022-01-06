b3e3b35aff3559689deec6abb559988ffa70f96e
Grégoire <72552360+Tuvedunyel@users.noreply.github.com>
2022-01-06 14:20:21 +0100

    footer socials

--- footer.php
+++ footer.php
@@ -2,15 +2,15 @@
     <section class="contact-info">
 
         <div class="container-narrow">
-           <?php if (get_field('phrase', 'options')): ?>
-                <h4><?php the_field('phrase', 'options'); ?></h4>
+            <?php if (get_field('phrase', 'options')): ?>
+            <h4><?php the_field('phrase', 'options'); ?></h4>
 
             <?php elseif (get_field('phrase_deux_couleurs', 'options')): ?>
-                <?php if (have_rows('phrase_deux_couleurs', 'options')) :
+            <?php if (have_rows('phrase_deux_couleurs', 'options')) :
                     while (have_rows('phrase_deux_couleurs', 'options')): the_row(); ?>
-                        <h4><?php the_sub_field('texte', 'options') ?></h4>
-                    <?php endwhile ?>
-                <?php endif ?>
+            <h4><?php the_sub_field('texte', 'options') ?></h4>
+            <?php endwhile ?>
+            <?php endif ?>
             <?php endif; ?>
         </div>
     </section>
@@ -22,20 +22,26 @@
 
     ?>
     <script>
-        const longitude = JSON.stringify(<?php echo $longitude; ?>);
-        const latitude = JSON.stringify(<?php echo $latitude; ?>);
-        const zoom = JSON.stringify(<?php echo $zoom; ?>);
-        const textMarker = JSON.stringify(<?php echo "'$textMarker'"; ?>);
+        const longitude = JSON.stringify( <?php echo $longitude; ?> );
+        const latitude = JSON.stringify( <?php echo $latitude; ?> );
+        const zoom = JSON.stringify( <?php echo $zoom; ?> );
+        const textMarker = JSON.stringify( <?php echo "'$textMarker'"; ?> );
+
     </script>
     <section class="map-container">
         <div id="map"></div>
     </section>
     <section class="footer-menu">
-        <div class="socials">
-            <h4><?= get_field('titre_socials', 'options') ?></h4>
-            <?php if (have_rows('socials', 'options')):
-                while (have_rows('socials', 'options')): the_row(); ?>
-                <?php 
+        <div class="container-narrow">
+
+
+            <div class="socials">
+                <h4><?= get_field('titre_socials', 'options') ?></h4>
+                <?php if (have_rows('socials', 'options')): ?>
+                <div class="socials-container">
+
+                    <?php while (have_rows('socials', 'options')): the_row(); ?>
+                    <?php 
                     $link = get_sub_field('lien_socials', 'options');
 
                     $link_url = $link['url'];
@@ -43,19 +49,20 @@
                     $link_target = $link['target'] ? $link['target'] : '_self';
                 ?>
                     <a href="<?= $link_url; ?>" title="<?= $link_title; ?>" target="<?= $link_target; ?>">
-                       <?php $image = get_sub_field('image_socials', 'options');
+                        <?php $image = get_sub_field('image_socials', 'options');
                         $url = $image['url'];
                         $alt = $image['alt'];
                         ?>
                         <img src="<?= esc_url($url); ?>" alt="<?= esc_attr($alt); ?>">
                     </a>
-                <?php endwhile ?>
-            <?php endif ?>
-        </div>
-        <div class="quick-access">
-            <h4><?= get_field('titre_acces_rapide', 'options'); ?></h4>
-            <nav id="footer-menu">
-                <?php
+                    <?php endwhile ?>
+                </div>
+                <?php endif ?>
+            </div>
+            <div class="quick-access">
+                <h4><?= get_field('titre_acces_rapide', 'options'); ?></h4>
+                <nav id="footer-navigation">
+                    <?php
                     wp_nav_menu( array(
                         'theme_location' => 'footer-menu',
                         'container' => false,
@@ -63,13 +70,44 @@
                         'fallback_cb' => false,
                         ) );
                 ?>
-            </nav>
+                </nav>
+            </div>
+            <div class="contact-details">
+                <h4><?= get_field('titre_contactez_nous', 'options'); ?></h4>
+                <address>
+                    <?php if (get_field('adresse', 'options')): ?>
+                    <strong><?= get_field('adresse', 'options'); ?></strong>
+                    <?php endif; ?>
+                    <?php if (get_field('telephone', 'options')): ?>
+                    <?php 
+                        $tel = get_field('lien_telephone', 'options');
+                        $tel_url = $tel['url'];
+                        $tel_title = $tel['title'];
+                    ?>
+                    <strong>Téléphone :</strong>
+                    <p><?= get_field('telephone', 'options'); ?></p>
+                    <?php endif; ?>
+                    <?php if (get_field('fax', 'options')) : ?>
+                    <?php 
+                        $fax = get_field('lien_fax', 'options');
+                        $fax_url = $fax['url'];
+                        $fax_title = $fax['title'];    
+                    ?>
+                    <strong>Fax :</strong>
+                    <p><?= get_field('fax', 'options'); ?></p>
+                    <?php endif; ?>
+                    <?php if (get_field('email', 'options')): ?>
+                    <strong>Mail :</strong>
+                    <a href="mailto:<?= get_field('email', 'options'); ?>"><?= get_field('email', 'options'); ?></a>
+                    <?php endif; ?>
+                </address>
+            </div>
         </div>
-        <div class="contact-details"></div>
     </section>
     <section class="legal-mention"></section>
 </footer>
 
 <?php wp_footer( ); ?>
 </body>
+
 </html>
