9fecbcef8667f702278b51aa8152ab9ad2ac60fc
Grégoire <72552360+Tuvedunyel@users.noreply.github.com>
2022-01-10 10:17:46 +0100

    contact page full

--- functions.php
+++ functions.php
@@ -33,3 +33,4 @@ if( function_exists( 'acf_add_options_page' ) ) {
 	) );
 	
 }
+add_filter('wpcf7_autop_or_not', '__return_false');
--- template-contact.php
+++ template-contact.php
@@ -1,24 +1,24 @@
 <?php
 /*
-
-    Template Name: Contact
-
+Template Name: Page Contact
 */
 get_header(); ?>
 
-<main>
-    <div class="hero-banner">
+<main class="contact-page">
+    <section class="hero-banner">
         <?php 
             if (get_field('hero_image')):
                 $hero_image = get_field('hero_image');
                 $hero_image_url = $hero_image['url'];
                 $hero_image_alt = $hero_image['alt']; ?>
-                <img src="<?= $hero_image_url ?>" alt="<?= $hero_image_alt ?>">
+                <div class="parallax-background">
+                    <img src="<?= $hero_image_url ?>" alt="<?= $hero_image_alt ?>">
+                </div>
             <?php endif; ?>
     </div>
     <section id="contact">
+        <h1><?php the_title(); ?></h1>
         <div class="container-narrow">
-            <h1><?php the_title(); ?></h1>
             <div class="contact-form__container">
                 <?php echo do_shortcode('[contact-form-7 id="26" title="Formulaire de contact 1"]'); ?>
             </div>
