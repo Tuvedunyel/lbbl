cbda6d0f9a8a6f8fe3919a13d32c2f22420cb8ad
Grégoire <72552360+Tuvedunyel@users.noreply.github.com>
2022-02-16 13:58:40 +0100

    cabinet desktop part 1

--- template-cabinet.php
+++ template-cabinet.php
@@ -4,6 +4,37 @@ Template Name: Cabinet
 */
 get_header(); ?>
 <main id="cabinet">
-    
+    <h1 class="screen-reader-text"><?php the_title(); ?></h1>
+    <section id="title-cabinet">
+        <div class="left-image">
+            <?php $image_cabinet = get_field('image_cabinet'); 
+            if( !empty($image_cabinet) ): ?>
+                <img class="image-cabinet" src="<?= $image_cabinet['url']; ?>" alt="<?= $image_cabinet['alt']; ?>" />
+            <?php endif; ?>
+        </div>
+        <div class="right-content-cabinet">
+            <?php $logoCabinet = get_field('logo_cabinet');
+            if( !empty($logoCabinet) ): ?>
+                <img class="logo-cabinet" src="<?= $logoCabinet['url']; ?>" alt="<?= $logoCabinet['alt']; ?>" />
+            <?php endif; ?>
+            <?php the_field('content'); ?>
+        </div>
+    </section>
+    <section class="suivi-contrat">
+        <div class="right-content-cabinet">
+            <?php the_field('contenu'); ?>
+        </div>
+        <div class="left-image">
+            <?php $image_suivi = get_field('image_suivi'); 
+            if( !empty($image_suivi) ): ?>
+                <img src="<?= $image_suivi['url']; ?>" alt="<?= $image_suivi['alt']; ?>" />
+            <?php endif; ?>
+        </div>
+    </section>
+    <div class="container-narrow">
+        <artile id="cabinet-content">
+            <?php the_content(); ?>
+        </artile>
+    </div>
 </main>	
 <?php get_footer(); ?>
