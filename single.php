f5e0615ccd23b921e3c211423e02efb087e65315
Grégoire <72552360+Tuvedunyel@users.noreply.github.com>
2022-02-17 10:24:38 +0100

    single

--- single.php
+++ single.php
@@ -10,103 +10,28 @@
             </div>
         </div>
     </section>
-    <article>
+    <section class="single-content-container">
         <div class="container-narrow">
-            <div class="content-wrapper">
-                <?php the_content(); ?>
-            </div>
-        </div>
-        <?php $type_page = get_field('page_simple_ou_complexe');
-        if ($type_page == 'complexe') : ?>
-        <div class="container-narrow page-complexe">
-            <section class="left-column">
-                <?php if(have_rows('colonne_de_gauche')): ?>
-                    <?php while(have_rows('colonne_de_gauche')) : the_row(); ?>
-                    <?php $couleur = get_sub_field('couleur_ou_blanc'); ?>
-                        <div id="<?= get_sub_field('ancre'); ?>" class="<?= $couleur ?>">
-                            <?php $image_left = get_sub_field('image'); 
-                                $image_left_url = $image_left['url'];
-                                $image_left_alt = $image_left['alt'];
-                            ?>
-                            <div class="image-container">
-                                <img src="<?= $image_left_url; ?>" alt="<?= $image_left_alt; ?>">
-                            </div>
-                            <div class="list-content">
-                                <h2><?php the_sub_field('titre'); ?></h2>
-                                <?php the_sub_field('texte'); ?>
-                            </div>
-                        </div>
-                    <?php endwhile; ?>
-                <?php endif; ?>
-            </section>
-            <section class="middle-column">
-                <?php if(have_rows('colonne_du_milieu')): ?>
-                    <?php while(have_rows('colonne_du_milieu')) : the_row(); ?>
-                    <?php $couleur = get_sub_field('couleur_ou_blanc'); ?>
-                        <div id="<?= get_sub_field('ancre'); ?>" class="<?= $couleur ?>">
-                            <?php $image_middle = get_sub_field('image'); 
-                                $image_middle_url = $image_middle['url'];
-                                $image_middle_alt = $image_middle['alt'];
-                            ?>
-                            <div class="image-container">
-                                <img src="<?= $image_middle_url; ?>" alt="<?= $image_middle_alt; ?>">
-                            </div>
-                            <div class="list-content">
-                                <h2><?php the_sub_field('titre'); ?></h2>
-                                <?php the_sub_field('texte'); ?>
-                            </div>
-                        </div>
-                    <?php endwhile; ?>
-                <?php endif; ?>
-            </section>
-            <section class="right-column">
-                <?php if(have_rows('colonne_de_droite')): ?>
-                    <?php while(have_rows('colonne_de_droite')) : the_row(); ?>
-                    <?php $couleur = get_sub_field('couleur_ou_blanc'); ?>
-                        <div id="<?= get_sub_field('ancre'); ?>" class="<?= $couleur ?>">
+            <?php if (have_rows('competences')):
+                while (have_rows('competences')): the_row(); ?>
+                    <article>
+                        <div class="single-img">
                             <?php $image = get_sub_field('image');
-                                $image_url = $image['url'];
-                                $image_alt = $image['alt'];
+                            $url = $image['url'];
+                            $alt = $image['alt'];
                             ?>
-                            <div class="image-container">
-                                <img src="<?= $image_url; ?>" alt="<?= $image_alt; ?>">
-                            </div>
-                            <div class="list-content">
-                                <h2><?php the_sub_field('titre'); ?></h2>
-                                <?php the_sub_field('texte'); ?>
-                            </div>
+                            <img src="<?= esc_url($url); ?>" alt="<?= esc_attr($alt); ?>">
                         </div>
-                    <?php endwhile; ?>
-                <?php endif; ?>
-            </section>
-        </div>
-
-        <?php else : ?>
-            <?php if (have_rows('liste')) : ?>
-                <div class="zone-ancre">
-                    <div class="container-narrow">
-                        <div class="top-left-border"></div>
-                        <?php while (have_rows('liste')) : the_row(); ?>
-                            <section id="<?= get_sub_field('ancre'); ?>">
-                            <div class="image-container">
-                                <?php $image = get_sub_field('image'); 
-                                    $image_url = $image['url'];
-                                    $image_alt = $image['alt'];
-                                ?>
-                                <img src="<?= $image_url; ?>" alt="<?= $image_alt; ?>">
-                            </div>    
-                                <div class="list-content">
-                                    <h2><?php the_sub_field('titre'); ?></h2>
-                                    <?php the_sub_field('contenu'); ?>
-                                </div>
-                            </section>
-                        <?php endwhile; ?>
-                        <div class="bottom-right-border"></div>
-                    </div>
-                </div>
+                        <div class="single-content">
+                            <h2><?php the_sub_field('titre'); ?></h2>
+                            <div class="single-text"><?php the_sub_field('texte'); ?></div>
+                        </div>
+                    </article>
+                <?php endwhile; ?>
             <?php endif; ?>
-        <?php endif; ?>
-    </article>
+
+        </div>
+    </section>
 </main>
 
 <?php get_footer(); ?>
